7c308111193f5c23db03714fe932be462235a412
emilrange <javaj@localhost.localdomain>
2016-09-13 19:04:50 +0200

    more login logic

--- src/main/java/club/domain/LoginUserBean.java
+++ src/main/java/club/domain/LoginUserBean.java
@@ -1,22 +1,24 @@
 package club.domain;
 
 import javax.inject.Named;
+import javax.validation.constraints.Size;
 
 import java.io.Serializable;
-
-import javax.enterprise.context.RequestScoped;
 import javax.enterprise.context.SessionScoped;
 
 @Named(value="loginUser")
 @SessionScoped
 public class LoginUserBean implements Serializable {
 
+	// TODO: tror inte vi kan blanda sessionscope och requestscope.
+	// därför måste vi sära på UserLogin och vanliga User. Kanske ska ha en frontend bean för varje action istället för entitet?
+	
 	private static final long serialVersionUID = -6514760360423746740L;
 
 	private String username;
-	private String password;
-
-	public String getUsername() {
+    private String password;
+    
+    public String getUsername() {
 		return username;
 	}
 	
@@ -32,7 +34,17 @@ public class LoginUserBean implements Serializable {
 		this.password = password;
 	}
 
+	public Boolean getValidLogin() {
+		return username != null;
+	}
+	
 	public String doLogin() {
+
+		//TODO: Vi får tyvärr inget frontend fel login misslyckades. fixa men hur? En validation-tag i html vore snyggt? Kan även diskuteras om session handler ska ligga så här nära frontend?
+		if(!(username.equals("emil") && password.equals("kaffe"))) { //TODO: hämta data från buisnesslagret?
+			username = null;
+			password = null;
+		}
 		
 		return "index";
 	}

e570ce09fe1a9b37e7b5bbb5fc7db598c2ba5348
Erik Wiberg <erik.wiberg.87@gmail.com>
2016-09-13 15:41:27 +0200

    add user management: list users, change "approved" status

--- src/main/java/club/EJB/interfaces/LocalUser.java
+++ src/main/java/club/EJB/interfaces/LocalUser.java
@@ -1,9 +1,13 @@
 package club.EJB.interfaces;
 
+import java.util.List;
+
 import javax.ejb.Local;
 import club.DAO.User;
 
 @Local
 public interface LocalUser {
 	boolean saveUser(User user);
+	List<User> getAll();
+	User getUserById(int selectedUserId);
 }

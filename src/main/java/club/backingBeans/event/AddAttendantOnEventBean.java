36c0a47f717b54374ee89797518eb222919e79cd
emilrange <javaj@localhost.localdomain>
2016-10-07 16:04:58 +0200

    attend button added that attend user

---
+++ src/main/java/club/backingBeans/event/AddAttendantOnEventBean.java
@@ -0,0 +1,46 @@
+package club.backingBeans.event;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import club.DAO.Event;
+import club.DAO.User;
+import club.backingBeans.EventBean;
+import club.backingBeans.PostGetterBean;
+import club.backingBeans.user.LoginUserBean;
+
+@Named(value="addAttendantOnEventBean")
+@RequestScoped
+public class AddAttendantOnEventBean extends EventBean {
+
+	@Inject @Named("loginUserBean")
+	private LoginUserBean loginUserBean;
+
+	@Inject @Named(value="postGetterBean")
+	private PostGetterBean postGetterBean;
+
+	public String submit() {
+		super.setId(postGetterBean.getAsEvent().getId());
+		return updateAndGetRedirect();		
+	}
+	
+	@Override
+	public Event getFromFields() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Event updateFromFields() {
+		
+		User userToAttend = loginUserBean.getUser();
+		if(userToAttend==null) throw new RuntimeException("Not logged in when try attend");
+		
+		Event eventToUpdate = (Event)super.getById(getId());
+		eventToUpdate.getAttendees().add(userToAttend);
+		//throw new RuntimeException("Not logged in when try attend");
+		return eventToUpdate;
+	}
+
+}

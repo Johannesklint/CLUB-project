2341b8e6dcdd6d2db8ae4b0f64df7c82c1c7ea09
Erik Wiberg <erik.wiberg.87@gmail.com>
2016-10-11 12:03:12 +0200

    add self-ref links to REST Response

--- src/main/java/club/resource/PostResource.java
+++ src/main/java/club/resource/PostResource.java
@@ -1,5 +1,8 @@
 package club.resource;
 
+import static javax.ws.rs.core.Response.Status.*;
+
+import java.net.URI;
 import java.util.List;
 
 import javax.ejb.EJB;
@@ -7,14 +10,15 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Link;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.UriInfo;
 
-import club.DAO.News;
-import club.DAO.User;
-import club.EJB.interfaces.LocalNews;
+import club.DAO.Post;
 import club.EJB.interfaces.LocalPost;
-import club.EJB.interfaces.LocalUser;
 
 @Path("/posts")
 public class PostResource {
@@ -22,20 +26,37 @@ public class PostResource {
 	@EJB
 	LocalPost postEJB;
 	
-	@EJB
-	LocalUser userEJB;
+	@Context
+	UriInfo uriInfo;
 	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getAllPosts(){
-		return Response.accepted(postEJB.getAll()).build();
+		List<Post> posts = postEJB.getAll();
+		return Response.status(Status.ACCEPTED)
+				.entity(posts)
+				.links(getSelfLink())
+				.build();
 	}
 	
 	@Path("/{id}")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getById(@PathParam("id") int id){
-		return Response.accepted(postEJB.getById(id)).build();
+		Post post = postEJB.getById(id);
+		return Response.status(ACCEPTED)
+				.entity(post)
+				.links(getSelfLink())
+				.build();
+	}
+	
+	private URI getAbsolutePathURIFromContext() {
+		return uriInfo.getAbsolutePath();
+	}
+	
+	private Link getSelfLink() {
+		URI uri = getAbsolutePathURIFromContext();
+		return Link.fromUri(uri).rel("self").build();
 	}
 	
 	

7d344f38f185407807260d14d6cadc15c7374a8c
emilrange <javaj@localhost.localdomain>
2016-10-14 17:02:56 +0200

    fix delete comment (not yet hide comment if not owner)

--- src/main/java/club/backingBeans/comment/DeleteCommentBean.java
+++ src/main/java/club/backingBeans/comment/DeleteCommentBean.java
@@ -2,9 +2,11 @@ package club.backingBeans.comment;
 
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 import club.DAO.Comment;
 import club.EJB.interfaces.LocalComment;
+import club.backingBeans.PostGetterBean;
 
 @Named(value="deleteCommentBean")
 @RequestScoped
@@ -13,33 +15,15 @@ public class DeleteCommentBean extends CommentBean{
 	@EJB
 	private LocalComment commentEJB;
 	
-	public String submit(){
-		return delete();
-	}
+	@Inject
+	private PostGetterBean postGetterBean;
 	
-	public String delete(){
-			System.out.println("DELETE FFS");
-		
+	public String submit(){
+
+		int postId = postGetterBean.getAsPost().getId();
 		Comment commentToDelete = commentEJB.getById(super.getSelectedCommentId());
-		
-			System.out.println("AFTER INIT COMMENTTODELTE - GET BY ID: " + super.getSelectedCommentId());
-		
 		commentToDelete.setHidden(true);
-		
-			System.out.println("AFTER SET HIDEN TO TRUE");
-		
-		boolean deletedComment = commentEJB.saveComment(commentToDelete);
-		
-			System.out.println("AFTER BOOLEAN DELETED-COMMENT");
-		
-		if(deletedComment){
-				System.out.println("AFTER IF TURNED TRUE");
-			
-			return "post-details.xhtml?faces-redirect=true&id=" + commentToDelete.getPost().getId();
-		}else{
-				System.out.println("AFTER IF TURNED FALSE");
-			
-			super.addFacesMessage("Could not delete");
-		}return "post-details.xhtml?faces-redirect=true&id=" + commentToDelete.getPost().getId();
+		commentEJB.saveComment(commentToDelete);
+		return "post-details.xhtml?faces-redirect=true&id=" + postId;
 	}
 }

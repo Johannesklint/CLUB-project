a8dbe0873d249bccc5c740e6f20b32a426950c46
Erik Wiberg <erik.wiberg.87@gmail.com>
2016-10-17 11:17:06 +0200

    add working (but uggly) datetime picker

--- src/main/java/club/backingBeans/EventBean.java
+++ src/main/java/club/backingBeans/EventBean.java
@@ -1,15 +1,15 @@
 package club.backingBeans;
-import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.Date;
 import java.util.List;
+
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
-import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import club.DAO.Event;
 import club.DAO.News;
-import club.DAO.Post;
 import club.DAO.User;
 import club.EJB.interfaces.LocalNews;
 import club.backingBeans.user.LoginUserBean;
@@ -18,10 +18,19 @@ import club.backingBeans.user.LoginUserBean;
 public abstract class EventBean extends PostBean<Event> {
 
 	private int selectedNewsId;
-	private Timestamp startTime;
+	
 	private int durationInMinutes;
 	private List<User> attendees;
+	private Date startTime = Date.from(Instant.now().minusSeconds(500 * 10000000));
 	
+	public Date getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
+
 	public List<User> getAttendees() {
 		return attendees;
 	}
@@ -61,13 +70,6 @@ public abstract class EventBean extends PostBean<Event> {
 		return ""; //TODO: do error handler
 	}
 			
-	public Timestamp getStartTime() {
-		return startTime;
-	}
-
-	public void setStartTime(Timestamp startTime) {
-		this.startTime = startTime;
-	}
 
 	public int getDurationInMinutes() {
 		return durationInMinutes;

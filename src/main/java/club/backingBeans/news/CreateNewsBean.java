42fa467e320b0c5cc97347d150226502ea26cc35
emilrange <javaj@localhost.localdomain>
2016-10-31 17:58:07 +0100

    fix form structure and style. fix author on news to work again

--- src/main/java/club/backingBeans/news/CreateNewsBean.java
+++ src/main/java/club/backingBeans/news/CreateNewsBean.java
@@ -9,6 +9,7 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import club.DAO.news.News;
+import club.DAO.user.User;
 import club.backingBeans.user.LoginUserBean;
 
 @RequestScoped
@@ -25,19 +26,22 @@ public class CreateNewsBean extends NewsBean {
 	
 	@PostConstruct
 	public void init() {
-		System.out.println("In CreateNewsBean init()");
 	}
 
 	@Override
-	public News updateFromFields() {
-		
+	public News updateFromFields() {		
 		return null;
 	}
-	
+
+	@Override
+	public User getAuthor() {
+		return loginUserBean.getUser();
+	}
+
 	@Override
 	public News getFromFields() {
 		News news = new News();
-		news.setAuthor(super.getAuthor());
+		news.setAuthor(this.getAuthor());  // note: must use this instead of super since getAuthor is overriden
 		news.setTitle(super.getTitle());
 		news.setText(super.getText());
 		news.setHidden(false);

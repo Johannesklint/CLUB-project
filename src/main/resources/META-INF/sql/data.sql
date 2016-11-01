ad9ed87fcd69b4904a7fa9f3991f9f2326ae4003
emilrange <javaj@localhost.localdomain>
2016-11-01 15:14:32 +0100

    style and name fixes. more user from start

--- src/main/resources/META-INF/sql/data.sql
+++ src/main/resources/META-INF/sql/data.sql
@@ -8,10 +8,20 @@ DROP FUNCTION IF EXISTS timestamp_plus_x_days_with_y_time;
 CREATE FUNCTION timestamp_plus_x_days_with_y_time(daysIntoFuture INTEGER, new_time VARCHAR(5)) RETURNS DATETIME RETURN DATE_ADD(DATE_ADD( CURRENT_DATE(), INTERVAL daysIntoFuture DAY), INTERVAL new_time HOUR_MINUTE);
 
 
-# Create Admins
+# Create Users - approved
 INSERT INTO `club_db`.`user` (`id`,`first_name`, `last_name`, `email`,`hmac_password`,`admin`, `approved_state`,`birthday`,`cpcid`) VALUES (null,'Admin', 'Adminsson', 'admin@admin.se', '73617059CED40DB461984E72F15EA0F2F30C2385565CDF59124B8C56911ECDC29E90328EF190E5D0EA339E7B1221A6F9BDC4BB5D25F14F560E7FA8B451931A0A:7B8509E35DD2EBBC00BAECA928F9CD57', true, 0,DATE('1985-06-03'),'admin');
-INSERT INTO `club_db`.`user` (`id`,`first_name`, `last_name`, `email`,`hmac_password`,`admin`, `approved_state`,`birthday`,`cpcid`) VALUES (null,'Marcin', 'Retek', 'marcin@retek.se', '73617059CED40DB461984E72F15EA0F2F30C2385565CDF59124B8C56911ECDC29E90328EF190E5D0EA339E7B1221A6F9BDC4BB5D25F14F560E7FA8B451931A0A:7B8509E35DD2EBBC00BAECA928F9CD57', true, 0,DATE('1991-06-07'),'marcinretek');
-INSERT INTO `club_db`.`user` (`id`,`first_name`, `last_name`, `email`,`hmac_password`,`admin`, `approved_state`,`birthday`,`cpcid`) VALUES (null,'Emil', 'Rånge', 'emil@range.se', '73617059CED40DB461984E72F15EA0F2F30C2385565CDF59124B8C56911ECDC29E90328EF190E5D0EA339E7B1221A6F9BDC4BB5D25F14F560E7FA8B451931A0A:7B8509E35DD2EBBC00BAECA928F9CD57', false, 2,DATE('1985-02-09'),'emilrange');
+INSERT INTO `club_db`.`user` (`id`,`first_name`, `last_name`, `email`,`hmac_password`,`admin`, `approved_state`,`birthday`,`cpcid`) VALUES (null,'Martin', 'Larsson', 'martin@larsson.se', '73617059CED40DB461984E72F15EA0F2F30C2385565CDF59124B8C56911ECDC29E90328EF190E5D0EA339E7B1221A6F9BDC4BB5D25F14F560E7FA8B451931A0A:7B8509E35DD2EBBC00BAECA928F9CD57', true, 0,DATE('1991-06-07'),'marcinretek');
+INSERT INTO `club_db`.`user` (`id`,`first_name`, `last_name`, `email`,`hmac_password`,`admin`, `approved_state`,`birthday`,`cpcid`) VALUES (null,'Emil', 'Jansson', 'emil@janson.se', '73617059CED40DB461984E72F15EA0F2F30C2385565CDF59124B8C56911ECDC29E90328EF190E5D0EA339E7B1221A6F9BDC4BB5D25F14F560E7FA8B451931A0A:7B8509E35DD2EBBC00BAECA928F9CD57', false, 0,DATE('1985-02-09'),'emilrange');
+
+# Create Users - waiting for approved
+INSERT INTO `club_db`.`user` (`id`,`first_name`, `last_name`, `email`,`hmac_password`,`admin`, `approved_state`,`birthday`,`cpcid`) VALUES (null,'Paul', 'Dianno', 'paul@dianno.se', '73617059CED40DB461984E72F15EA0F2F30C2385565CDF59124B8C56911ECDC29E90328EF190E5D0EA339E7B1221A6F9BDC4BB5D25F14F560E7FA8B451931A0A:7B8509E35DD2EBBC00BAECA928F9CD57', true, 2,DATE('1991-06-07'),'marcinretek');
+INSERT INTO `club_db`.`user` (`id`,`first_name`, `last_name`, `email`,`hmac_password`,`admin`, `approved_state`,`birthday`,`cpcid`) VALUES (null,'Jon', 'English', 'jon@english.se', '73617059CED40DB461984E72F15EA0F2F30C2385565CDF59124B8C56911ECDC29E90328EF190E5D0EA339E7B1221A6F9BDC4BB5D25F14F560E7FA8B451931A0A:7B8509E35DD2EBBC00BAECA928F9CD57', false, 2,DATE('1985-02-09'),'emilrange');
+INSERT INTO `club_db`.`user` (`id`,`first_name`, `last_name`, `email`,`hmac_password`,`admin`, `approved_state`,`birthday`,`cpcid`) VALUES (null,'Lisa', 'Steinman', 'lisa@steinman.se', '73617059CED40DB461984E72F15EA0F2F30C2385565CDF59124B8C56911ECDC29E90328EF190E5D0EA339E7B1221A6F9BDC4BB5D25F14F560E7FA8B451931A0A:7B8509E35DD2EBBC00BAECA928F9CD57', true, 2,DATE('1991-06-07'),'marcinretek');
+
+# Create Users - denied
+INSERT INTO `club_db`.`user` (`id`,`first_name`, `last_name`, `email`,`hmac_password`,`admin`, `approved_state`,`birthday`,`cpcid`) VALUES (null,'Tina', 'Hill', 'tina@hill.se', '73617059CED40DB461984E72F15EA0F2F30C2385565CDF59124B8C56911ECDC29E90328EF190E5D0EA339E7B1221A6F9BDC4BB5D25F14F560E7FA8B451931A0A:7B8509E35DD2EBBC00BAECA928F9CD57', false, 1,DATE('1985-02-09'),'emilrange');
+INSERT INTO `club_db`.`user` (`id`,`first_name`, `last_name`, `email`,`hmac_password`,`admin`, `approved_state`,`birthday`,`cpcid`) VALUES (null,'Karl', 'McFly', 'karl@mcfly.se', '73617059CED40DB461984E72F15EA0F2F30C2385565CDF59124B8C56911ECDC29E90328EF190E5D0EA339E7B1221A6F9BDC4BB5D25F14F560E7FA8B451931A0A:7B8509E35DD2EBBC00BAECA928F9CD57', true, 1,DATE('1991-06-07'),'marcinretek');
+INSERT INTO `club_db`.`user` (`id`,`first_name`, `last_name`, `email`,`hmac_password`,`admin`, `approved_state`,`birthday`,`cpcid`) VALUES (null,'Billy', 'Plant', 'billy@plant.se', '73617059CED40DB461984E72F15EA0F2F30C2385565CDF59124B8C56911ECDC29E90328EF190E5D0EA339E7B1221A6F9BDC4BB5D25F14F560E7FA8B451931A0A:7B8509E35DD2EBBC00BAECA928F9CD57', false, 1,DATE('1985-02-09'),'emilrange');
 
 # Create Theme
 
